6ee8fdf29d4e4c8de084791eeb9e45c09c4d2813
Leo <leoli_082000@hotmail.com>
2026-08-01 01:49:19 +0800

    Conform opaque sandbox repro config to Playwright lint

--- repros/service-workers-block-opaque-sandbox/playwright.config.ts
+++ repros/service-workers-block-opaque-sandbox/playwright.config.ts
@@ -1,15 +1,29 @@
-import { defineConfig } from "@playwright/test";
+// Copyright (c) Microsoft Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: ".",
+  testDir: '.',
   workers: 1,
   retries: 0,
-  reporter: "line",
+  reporter: 'line',
   use: {
-    browserName: "chromium",
-    serviceWorkers: "block",
-    trace: "off",
-    screenshot: "off",
-    video: "off"
-  }
+    browserName: 'chromium',
+    serviceWorkers: 'block',
+    trace: 'off',
+    screenshot: 'off',
+    video: 'off',
+  },
 });
